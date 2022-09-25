da0d8b7114c4a76196f785a5e17d0c9c12466cc9
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-09-24 23:04:39 -0400

    Added another route for registering and updated the route for getting users.

--- server/src/controllers/auth-controllers.js
+++ server/src/controllers/auth-controllers.js
@@ -1,20 +1,31 @@
 const db = require('../db');
+const { hash } = require('bcryptjs');
 
 exports.getUsers = async (req, res) => {
 	let rows;
 	try {
-		rows = await db.query('select * from users');
-		console.log(response);
+		rows = await db.query('SELECT user_id, email, created_at FROM users');
 	} catch (error) {
 		console.log(error.message);
 	}
-	res.status(200).json({ response: rows.rows });
+	res.status(200).json({ users: rows.rows });
 };
 
 exports.register = async (req, res) => {
+	const { email, password } = req.body;
 	try {
-		console.log('Valdiation past');
+		const hashedPassword = await hash(password, 10);
+		await db.query('INSERT INTO users(email, password) VALUES($1, $2)', [
+			email,
+			hashedPassword,
+		]);
 	} catch (error) {
-		console.log(error.message);
+		res.status(500).json({
+			error: error.message,
+		});
 	}
+	res.status(201).json({
+		success: true,
+		message: 'Your account has successfully been created.',
+	});
 };
